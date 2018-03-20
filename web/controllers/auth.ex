0dd8724b7dab8ead688499ef71ffa2c6ecb71f0e
Rafael Ruiz <rmoreno@competa.com>
2018-03-20 09:10:28 +0100

    Add videos and categories

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,6 +1,10 @@
 defmodule Rumbl.Auth do
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
+
+  alias Rumbl.Router.Helpers
+
 
   def init(opts) do
     Keyword.fetch!(opts, :repo)
@@ -10,6 +14,17 @@ defmodule Rumbl.Auth do
     user_id = get_session(conn, :user_id)    
     user = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
+  end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged to access that page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
   end
 
   def login(conn, user) do
